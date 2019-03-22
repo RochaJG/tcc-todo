c9eee3ae8cec76e22fad0b9894d14ff5437e2823
Jordan Rocha <70rd23@gmail.com>
2019-03-22 00:30:07 -0300

    Deploy da aplicação

---
+++ build/react/precache-manifest.825d2192c0d998820ae34865b8f5dde0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb0de6c6e333c3a680cb",
+    "url": "/static/js/main.5efa2e84.chunk.js"
+  },
+  {
+    "revision": "d3b0d6416a646ffa3fa6",
+    "url": "/static/js/2.6d577bb4.chunk.js"
+  },
+  {
+    "revision": "bb0de6c6e333c3a680cb",
+    "url": "/static/css/main.64e1c6a5.chunk.css"
+  },
+  {
+    "revision": "cf84d6c7f0ecb5400290d7692aab8d5e",
+    "url": "/index.html"
+  }
+];
